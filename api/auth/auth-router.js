604d86baee95b9fe58722bce2042e6a4204c9801
ShariqAli-Dev <shariqali.dev@gmail.com>
2021-11-06 17:05:34 -0400

    completed assigmnet

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -18,18 +18,6 @@ router.post('/register', validateRoleName, (req, res, next) => {
       res.status(201).json({ savedUser });
     })
     .catch(next);
-
-  /**
-    [POST] /api/auth/register { "username": "anna", "password": "1234", "role_name": "angel" }
-
-    response:
-    status 201
-    {
-      "user"_id: 3,
-      "username": "anna",
-      "role_name": "angel"
-    }
-   */
 });
 
 router.post('/login', checkUsernameExists, (req, res, next) => {
@@ -48,25 +36,6 @@ router.post('/login', checkUsernameExists, (req, res, next) => {
       }
     })
     .catch(next);
-  /**
-    [POST] /api/auth/login { "username": "sue", "password": "1234" }
-
-    response:
-    status 200
-    {
-      "message": "sue is back!",
-      "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ETC.ETC"
-    }
-
-    The token must expire in one day, and must provide the following information
-    in its payload:
-
-    {
-      "subject"  : 1       // the user_id of the authenticated user
-      "username" : "bob"   // the username of the authenticated user
-      "role_name": "admin" // the role of the authenticated user
-    }
-   */
 });
 
 const makeToken = (user) => {
